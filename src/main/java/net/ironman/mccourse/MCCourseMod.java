f403e0b2df074d01e10940e56009109c65e69404
IronMan2297 <liamweigel1@icloud.com>
2023-08-09 22:44:58 +0200

    new

--- src/main/java/net/ironman/mccourse/MCCourseMod.java
+++ src/main/java/net/ironman/mccourse/MCCourseMod.java
@@ -1,6 +1,8 @@
 package net.ironman.mccourse;
 
 import com.mojang.logging.LogUtils;
+import net.ironman.mccourse.item.ModItems;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -16,13 +18,15 @@ import org.slf4j.Logger;
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(MCCourseMod.MOD_ID)
 public class MCCourseMod {
-    // Define mod id in a common place for everything to reference
     public static final String MOD_ID = "mccourse";
-    // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
 
     public MCCourseMod() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+
+        ModItems.register(modEventBus);
+
 
         modEventBus.addListener(this::commonSetup);
 
@@ -34,9 +38,11 @@ public class MCCourseMod {
 
     }
 
-    // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
-
+        if (event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.ALEXANDRITE);
+            event.accept(ModItems.RAW_ALEXANDRITE);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
