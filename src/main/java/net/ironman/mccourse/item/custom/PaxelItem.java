9b9062f0c53401c924bd4ce30ac26df8ec270939
IronMan2297 <liamweigel1@icloud.com>
2023-08-12 20:54:58 +0200

    new

--- src/main/java/net/ironman/mccourse/item/custom/PaxelItem.java
+++ src/main/java/net/ironman/mccourse/item/custom/PaxelItem.java
@@ -1,11 +1,9 @@
 package net.ironman.mccourse.item.custom;
 
 import net.ironman.mccourse.util.ModTags;
-import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.DiggerItem;
 import net.minecraft.world.item.Tier;
 import net.minecraft.world.item.Vanishable;
-import net.minecraft.world.level.block.Block;
 
 public class PaxelItem extends DiggerItem implements Vanishable {
     public PaxelItem(Tier pTier, float pAttackDamageModifier, float pAttackSpeedModifier, Properties pProperties) {
