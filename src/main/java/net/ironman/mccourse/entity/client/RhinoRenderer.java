a3400ac5aff3aec4a1c3984beb774fafca4ef8c3
IronMan2297 <liamweigel1@icloud.com>
2023-08-24 00:12:59 +0200

    Rhinos can now be white

--- src/main/java/net/ironman/mccourse/entity/client/RhinoRenderer.java
+++ src/main/java/net/ironman/mccourse/entity/client/RhinoRenderer.java
@@ -1,15 +1,27 @@
 package net.ironman.mccourse.entity.client;
 
+import com.google.common.collect.Maps;
 import com.mojang.blaze3d.vertex.PoseStack;
 import net.ironman.mccourse.MCCourseMod;
 import net.ironman.mccourse.entity.custom.RhinoEntity;
 import net.ironman.mccourse.entity.layers.ModModelLayers;
+import net.ironman.mccourse.entity.variant.RhinoVariant;
+import net.minecraft.Util;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.renderer.entity.MobRenderer;
 import net.minecraft.resources.ResourceLocation;
 
+import java.util.Map;
+
 public class RhinoRenderer extends MobRenderer<RhinoEntity, RhinoModel<RhinoEntity>> {
+    private static final Map<RhinoVariant, ResourceLocation> LOCATION_BY_VARIANT =
+            Util.make(Maps.newEnumMap(RhinoVariant.class), map -> {
+                map.put(RhinoVariant.DEFAULT,
+                        new ResourceLocation(MCCourseMod.MOD_ID, "textures/entity/rhino.png"));
+                map.put(RhinoVariant.WHITE,
+                        new ResourceLocation(MCCourseMod.MOD_ID, "textures/entity/white_rhino.png"));
+            });
     private static final ResourceLocation RHINO_LOCATION = new ResourceLocation(MCCourseMod.MOD_ID, "textures/entity/rhino.png");
 
     public RhinoRenderer(EntityRendererProvider.Context pContext) {
@@ -18,7 +30,7 @@ public class RhinoRenderer extends MobRenderer<RhinoEntity, RhinoModel<RhinoEnti
 
     @Override
     public ResourceLocation getTextureLocation(RhinoEntity pEntity) {
-        return RHINO_LOCATION;
+        return LOCATION_BY_VARIANT.get(pEntity.getVariant());
     }
 
     @Override
